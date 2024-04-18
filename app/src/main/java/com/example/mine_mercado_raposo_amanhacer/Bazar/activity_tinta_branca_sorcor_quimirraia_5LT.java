d5902a5994caa7145aa92fa0c0152a036510c81b
rodrigo2345 <mr_rodrigo3@hotmail.com>
2024-04-18 12:09:41 +0100

    novo

---
+++ app/src/main/java/com/example/mine_mercado_raposo_amanhacer/Bazar/activity_tinta_branca_sorcor_quimirraia_5LT.java
@@ -0,0 +1,67 @@
+package com.example.mine_mercado_raposo_amanhacer.Bazar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.mine_mercado_raposo_amanhacer.R;
+import com.example.mine_mercado_raposo_amanhacer.data.localdatabase.Contact;
+import com.example.mine_mercado_raposo_amanhacer.view.ContactAdapter;
+import com.example.mine_mercado_raposo_amanhacer.view.MainActivity;
+import com.example.mine_mercado_raposo_amanhacer.view.PesquisaActivity;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class activity_tinta_branca_sorcor_quimirraia_5LT extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ContactAdapter adapter;
+    private List<Contact> carrinhoList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinta_branca_sorcor_quimirraia5_lt);
+        List<Contact> contactList = new ArrayList<>();
+        contactList.add(new Contact("Tinta Branca Sorcor Quimirraia 5LT", "17,65€","" , R.drawable.tinta_branca_sorcor_quimirraia5_lt, 1));
+        recyclerView = findViewById(R.id.recyclerView_product77);
+        carrinhoList = getCarrinhoListFromSharedPreferences();
+
+        adapter = new ContactAdapter(this, contactList, carrinhoList);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
+    }
+
+    private List<Contact> getCarrinhoListFromSharedPreferences() {
+        SharedPreferences preferences = getSharedPreferences("Carrinho", Context.MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = preferences.getString("carrinhoList", null);
+        if (json != null) {
+            Type type = new TypeToken<ArrayList<Contact>>() {
+            }.getType();
+            return gson.fromJson(json, type);
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    public void Search(View view) {
+        Intent intent = new Intent(activity_tinta_branca_sorcor_quimirraia_5LT.this, PesquisaActivity.class);
+        startActivity(intent);
+    }
+
+    public void Home(View view) {
+        Intent intent = new Intent(activity_tinta_branca_sorcor_quimirraia_5LT.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
